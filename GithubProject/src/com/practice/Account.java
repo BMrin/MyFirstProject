093c232774cf329b10930c3fbb9a1d496ecbdd58
mrinmoy.brahma <mrinmoy.brahma@WKSBAN24KAN0016.corp.mphasis.com>
2019-10-26 10:15:48 +0530

    Mrinmoy Modify

---
+++ GithubProject/src/com/practice/Account.java
@@ -0,0 +1,23 @@
+package com.practice;
+
+public class Account {
+
+	//Modify Mrinmoy
+	
+	int acno;
+	String acname;
+	
+	public int getAcno() {
+		return acno;
+	}
+	public void setAcno(int acno) {
+		this.acno = acno;
+	}
+	public String getAcname() {
+		return acname;
+	}
+	public void setAcname(String acname) {
+		this.acname = acname;
+	}
+		
+}
